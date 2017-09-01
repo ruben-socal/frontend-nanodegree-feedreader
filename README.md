f8bb41b000f444906053d8d2a9b8dcb1bfec6433
Ruben <ruben2045@gmail.com>
2017-09-01 16:54:43 -0700

    fix: corrections made on 'The Menu' suite on default hide menu test, 'Initial Entries' suite on 'loadFeed function completes with a single entry in feed comtainer' and 'New Feed Selections' suite on 'loadFeed function changes content on feed' on feedreader.js docs: remove TODO documetaion and update README with 'start instructions'

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 Jasmine is a Javascript Testing Framework [Jasmine documentation](http://jasmine.github.io). Jasmine was used to run tests on the Feed Reader application that will be listed below.
 
+## Getting Started
+
+1. Download the files from [link to github!](https://github.com/ruben-socal/frontend-nanodegree-feedreader)
+2. Once the link is open, click on green button that says **clone or dowwload**
+3. Once you click on the green button, choose **Download zip**
+4. Once the zip file has been downloaded, click on zip file and extract files. [extract files on windows](https://support.microsoft.com/en-us/help/14200/windows-compress-uncompress-zip-files), [extract files on a Mac](http://support.topspinmedia.com/hc/en-us/articles/204262713-How-to-extract-a-zip-file-on-a-Mac)
+2. Locate extracted files directory: **frontend-nanodegree-feedreader**
+3. Open index.html file by double clicking on it
+4. Once the index.html file is open its ready to use.
+5. The list of tests that have completed and passed can be seen at the bottom of the webpage.
+
 ## RSS Feeds Suite
 1. Created test suite named `"RSS Feeds"`.
 2. This test loops through each feed in the `allFeeds` object and ensures it has a URL defined and that the URL is not empty.
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* This is the second test - it loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -43,7 +43,7 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* This is the third test - it loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -60,21 +60,21 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* This test suite is named "The menu" */
     describe('The Menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
+        /* This is the fourth test - it ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
         it('default hide menu is defined', function() {
             // css hides menu when menu-hidden and slide-menu are visible
-            var hideMenu = $('.menu-hidden .slide-menu');
-            expect(hideMenu).toBeDefined();
+            var isHidden = $('.menu-hidden').is( ":hidden" );
+            expect(isHidden).toBe(false);
         });
 
-         /* TODO: Write a test that ensures the menu changes
+         /* This is the fifth test - it ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
@@ -89,44 +89,48 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* This test suite is named "Initial Entries" */
     describe('Initial Entries', function() {
-        /* TODO: Write a test that ensures when the loadFeed
+        /* This is the sixth test - it ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        // var feedEntry;
         beforeEach(function(done) {
             loadFeed(0,done);
         });
-        it('loadFeed function is called and completed is defined', function(done) {
-            var feedEntry = $('.entry').html();
-            expect(feedEntry).toBeDefined();
+        it('loadFeed function completes with a single entry in feed comtainer', function(done) {
+            var feedEntry = $('.feed .entry').html();
+            expect(feedEntry).not.toBe('');
             done();
         });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+    /* This test suite named is "New Feed Selection"
        I was able to complete this problem from the help of the
        Udacity forum at the following link:
        https://discussions.udacity.com/t/p6-new-feed-selection-test-question-problem/15562/2
     */
     describe('New Feed Selection', function() {
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* This is the seventh test - it ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-        var oldFeed;
+        var oldFeed, newFeed;
         beforeEach(function(done) {
-            loadFeed(1,done);
-            oldFeed = $('.feed').html();
+            loadFeed(0, function() {
+                oldFeed = $('.feed').html();
+                done();
+            });
         });
-        it('new feed is loaded by loadFeed function is defined', function(done) {
-            loadFeed(0);
-            var newFeed = $('.feed').html();
-            expect(newFeed).not.toEqual(oldFeed);
-            done();
+        it('loadFeed function changes content on feed', function(done) {
+            loadFeed(1, function() {
+                newFeed = $('.feed').html();
+                done();
+            });
+            expect(newFeed).not.toBe(oldFeed);
         });
     });
 
